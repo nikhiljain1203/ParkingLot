86b69bd8954d171f68b31c0f98f0f6b85c5234b3
nikhiljain1203 <121147663+nikhiljain1203@users.noreply.github.com>
2023-03-20 21:05:32 +0530

    Initial Commit

---
+++ ParkingLot/src/main/java/com/scaler/parkinglot/Main.java
@@ -0,0 +1,9 @@
+package com.scaler.parkinglot;
+
+// Will Start at 9.10 
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
---
+++ ParkingLot/src/main/java/com/scaler/parkinglot/strategies/spotassignment/RandomSpotAssignmentStrategy.java
@@ -0,0 +1,28 @@
+package com.scaler.parkinglot.strategies.spotassignment;
+
+import com.scaler.parkinglot.models.*;
+import com.scaler.parkinglot.repositories.ParkingLotRepository;
+
+public class RandomSpotAssignmentStrategy implements SpotAssignmentStrategy {
+
+    private ParkingLotRepository parkingLotRepository;
+
+    public RandomSpotAssignmentStrategy(ParkingLotRepository parkingLotRepository) {
+        this.parkingLotRepository = parkingLotRepository;
+    }
+
+    @Override
+    public ParkingSpot assignSpot(VehicleType vehicleType, Gate gate) {
+//        ParkingLot parkingLot = ParkingLotRepository.getParkingLotWithGate();
+//        List<ParkingSpot> parkingSpots = ParkingSpotRepository.getParkingSpotsByLot();
+//
+//        for (ParkingSpot parkingSpot: parkingSpots) {
+//            if (parkingSpot.getParkingSpotStatus().equals(ParkingSpotStatus.AVAILABLE) && parkingSpot.getSupportedVehicleTypes().contains(vehicleType)) {
+//                return parkingSpot;
+//            }
+//        }
+//        return null;
+
+        return null;
+    }
+}
